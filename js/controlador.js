a51556220152271b32546e5934b90a4830a9c1b2
Darknessteel <jaimeandresvargas@hotmail.com>
2020-09-27 03:03:31 -0500

    Initial commit

---
+++ js/controlador.js
@@ -0,0 +1,17 @@
+//entradas
+let nombreUsuarioUno="Juan";
+let edad=5000;
+let estaturaUsuario=1.62;
+let estaVivo=true;
+
+const PI=3.141516;
+
+//salidas
+console.log("Hello World");
+console.log("Hola como te va"+nombreUsuarioUno);
+console.log(`Hola como te va ${nombreUsuarioUno} Hoy es jueves y tienes esta edad: ${edad}`);
+
+console.log(`El número PI equivale a: ${PI}`)
+edad=15;
+
+console.log(`La edad del usuario equivale a: ${edad}`)
